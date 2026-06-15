f35a68e66ada3108d59490c46ee72b899a35f0c7
Shekh-Taukir <shekhtaukir13@gmail.com>
2026-06-15 13:51:26 +0530

    ER 1000 - Initial commit
    Actual Change Detail:
    - Initial commit for v2 of IMS

---
+++ IMS_v2/IMS_v2/src/main/java/com/tsTech/practice/IMS_v2/ImsV2Application.java
@@ -0,0 +1,13 @@
+package com.tsTech.practice.IMS_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImsV2Application.class, args);
+	}
+
+}
---
+++ IMS_v2/IMS_v2/src/test/java/com/tsTech/practice/IMS_v2/ImsV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsTech.practice.IMS_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImsV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
